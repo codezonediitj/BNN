ca57165f0df8215e57c7ab93e209ac3ddc5ed0c3
czgdp1807 <singh.23@iitj.ac.in>
2020-04-27 23:42:41 +0530

    ready

--- bnn/templates/autodiff/forward.hpp
+++ bnn/templates/autodiff/forward.hpp
@@ -25,5 +25,17 @@ template TensorCPU<unsigned long long>** compute_gradient_forward<unsigned long 
 template TensorCPU<float>** compute_gradient_forward<float>(Operator<float>* expr, TensorCPU<float>** vars, unsigned num_vars);
 template TensorCPU<double>** compute_gradient_forward<double>(Operator<double>* expr, TensorCPU<double>** vars, unsigned num_vars);
 template TensorCPU<long double>** compute_gradient_forward<long double>(Operator<long double>* expr, TensorCPU<long double>** vars, unsigned num_vars);
+template TensorCPU<bool>* compute_value<bool>(Operator<bool>* expr);
+template TensorCPU<short>* compute_value<short>(Operator<short>* expr);
+template TensorCPU<unsigned short>* compute_value<unsigned short>(Operator<unsigned short>* expr);
+template TensorCPU<int>* compute_value<int>(Operator<int>* expr);
+template TensorCPU<unsigned int>* compute_value<unsigned int>(Operator<unsigned int>* expr);
+template TensorCPU<long>* compute_value<long>(Operator<long>* expr);
+template TensorCPU<unsigned long>* compute_value<unsigned long>(Operator<unsigned long>* expr);
+template TensorCPU<long long>* compute_value<long long>(Operator<long long>* expr);
+template TensorCPU<unsigned long long>* compute_value<unsigned long long>(Operator<unsigned long long>* expr);
+template TensorCPU<float>* compute_value<float>(Operator<float>* expr);
+template TensorCPU<double>* compute_value<double>(Operator<double>* expr);
+template TensorCPU<long double>* compute_value<long double>(Operator<long double>* expr);
 
 #endif
