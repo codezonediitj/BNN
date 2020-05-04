5b79cf14a809aa0e9dc48063550f99e9f203b9d8
czgdp1807 <singh.23@iitj.ac.in>
2020-05-04 16:23:28 +0530

    docs added

--- bnn/io/input.hpp
+++ bnn/io/input.hpp
@@ -10,11 +10,27 @@ namespace bnn
 
         using namespace bnn::core;
 
+        /*
+        * Loads MNIST image file into bnn::core::TensorCPU.
+        *
+        * @tparam data_type Data type of the elements
+        *     supported by C++.
+        * @param path const string& Path to the MNIST image
+        *     file.
+        */
         template <class data_type>
         TensorCPU<data_type>*
         load_mnist_images
         (const string& path);
 
+        /*
+        * Loads MNIST label file into bnn::core::TensorCPU.
+        *
+        * @tparam data_type Data type of the elements
+        *     supported by C++.
+        * @param path const string& Path to the MNIST label
+        *     file.
+        */
         template <class data_type>
         TensorCPU<data_type>*
         load_mnist_labels
