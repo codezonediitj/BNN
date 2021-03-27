39ced589caf1b266532e85df2e11196badfa441e
czgdp1807 <gdp.1807@gmail.com>
2021-03-27 20:50:59 +0530

    Layer Addition started

---
+++ bnn/nn/layers.hpp
@@ -0,0 +1,22 @@
+#ifndef BNN_BNN_NN_LAYERS_HPP
+#define BNN_BNN_NN_LAYERS_HPP
+
+#include <bnn/utils/utils.hpp>
+
+namespace bnn
+{
+    namespace nn
+    {
+
+        using namespace bnn::utils;
+
+        template <class data_type>
+        class Layer: public BNNBase
+        {
+
+        };
+
+    }
+}
+
+#endif
